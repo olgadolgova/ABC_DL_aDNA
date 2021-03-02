5dd097df0dd5996e6f648c5e05c9b1504d37e8b4
olgadolgova <olgadolgova@inbox.ru>
2021-03-02 13:01:17 +0100

    Update abcModel.R

--- abcModel.R
+++ abcModel.R
@@ -4,11 +4,7 @@ if("abc" %in% rownames(installed.packages()) == FALSE)
  stop("abc package is required"); 
 }
 library("abc"); # load the abc package
-<<<<<<< Updated upstream:abcModel.R
-abc_folder <- “path_to_abc_dl”; # CHANGE TO THE FOLDER WHERE YOU RUN THE SIMULATIONS
-=======
-abc_folder <-"Path/to/your/directory"; # CHANGE TO THE FOLDER WHERE YOU RUN THE SIMULATIONS
->>>>>>> Stashed changes:ABC_DL_Example_Project/abcModel.R
+abc_folder <-"C:/Users/odolgova/OneDrive - CRG - Centre de Regulacio Genomica/Olga/Bronze Age Greece/Final_corrections/ABC-DL_OL_github/ABC_DL-master/ABC_DL_Example_Project/test_model"; # CHANGE TO THE FOLDER WHERE YOU RUN THE SIMULATIONS
 # now, for each of the neural network replicates, compute the mean distance. Return the sum
 compute.mean.nn <- function(simulated.ss, nen, n.models)
 {
@@ -22,11 +18,7 @@ compute.mean.nn <- function(simulated.ss, nen, n.models)
   return(simulated.ss.by.nn/nen);
 }
 # data with the model predictions from the DL
-<<<<<<< Updated upstream:abcModel.R
-data.t <- read.table(file=paste(abc_folder,”model_predictions.txt”,sep=”\\”),header=F);
-=======
 data.t <- read.table(file=paste("model_predictions.txt"),header=F);
->>>>>>> Stashed changes:ABC_DL_Example_Project/abcModel.R
 # first row is the observed data
 observed.ss <- data.t[which(data.t[,1]=="observed"),2:ncol(data.t)]
 # summary statistics of the simulated data. Remember. If we have K models, each K columns correspond to a NN prediction.
@@ -51,11 +43,7 @@ res <- postpr(mean.observed.ss, models, mean.ss, tol=1000/nrow(simulated.ss),met
 summary(res)
 model.names
 
-<<<<<<< Updated upstream:abcModel.R
-#To calculate the confusion matrix out of 100 simulations per model to see to which extend the simulations generated under particular model (rows) are properly assigned to this model (columns)
-=======
 
->>>>>>> Stashed changes:ABC_DL_Example_Project/abcModel.R
 confussion.matrix <- matrix(nrow=length(model.names), ncol=length(model.names),rep(0,length(model.names)*length(model.names)));
 rownames(confussion.matrix) <- as.character(model.names);
 colnames(confussion.matrix) <- as.character(model.names);
@@ -81,7 +69,4 @@ for(m in 1:length(model.names))
 }
 confussion.matrix
 
-<<<<<<< Updated upstream:abcModel.R
-=======
 
->>>>>>> Stashed changes:ABC_DL_Example_Project/abcModel.R
